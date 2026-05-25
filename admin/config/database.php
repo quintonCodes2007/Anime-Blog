b500618b2f64bbbd50c5aae9d6b4c57ecfd2cfd5
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-05-25 15:25:32 +0200

    changes

--- admin/config/database.php
+++ admin/config/database.php
@@ -5,8 +5,6 @@ require 'constants.php';
 //connecting to the database
 $connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-if(mysqli_errno($connection)) {
+if (mysqli_errno($connection)) {
 	die(mysqli_error($connection));
 }
-
-
